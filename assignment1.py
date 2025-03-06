8377b031f35b055de0d9734d7654f16cb87ec7ba
ms.choi <myeongseo.choi@my.jci.edu>
2025-03-06 12:32:11 +0800

    compose function

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,33 @@
 """
-Name:
-Date started:
-GitHub URL:
+Name: Choi Myeongseo
+Date started: 5 March 2025
+GitHub URL: https://github.com/myoungseo/assignment1
 """
+import csv
 
+FILENAME = "songs.csv"
 
 def main():
     """..."""
-    print("Song List 1.0 - by Your Name")
+    print("Song List 1.0 - by Choi Myeongseo")
 
+def load_songs(FILENAME):
+    """Load songs from csv file"""
+
+def save_songs(songs):
+    """Save songs to a csv file"""
+
+def display_songs(songs):
+    """Display songs sorted by year and title."""
+
+def add_songs(songs):
+    """Add a new song to the song list."""
+
+def complete_songs(songs):
+    """Mark a song as learned."""
+
+def get_valid_input():
+    """Generic input validation function."""
 
 if __name__ == '__main__':
     main()
