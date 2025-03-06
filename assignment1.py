179cb5fefe06c27658555a48ffbd1c8284542a51
ms.choi <myeongseo.choi@my.jci.edu>
2025-03-06 16:27:57 +0800

    function save songs and display songs

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,9 @@ Date started: 5 March 2025
 GitHub URL: https://github.com/myoungseo/assignment1
 """
 import csv
-
 FILENAME = "songs.csv"
+LEARNED = "l"
+UNLEARNED = "u"
 
 def main():
     """..."""
@@ -26,9 +27,23 @@ def load_songs(FILENAME):
 
 def save_songs(songs):
     """Save songs to a csv file"""
+    with open(FILENAME, "w") as file:
+        for song in songs:
+            file.write(f"{song[0]},{song[1]},{song[2]},{song[3]}\n")
 
 def display_songs(songs):
     """Display songs sorted by year and title."""
+    from operator import itemgetter
+    songs.sort(key=itemgetter(0))
+    songs.sort(key=itemgetter(2))
+
+    unlearned_count = sum(1 for song in songs if song[3] == UNLEARNED)
+
+    for i, song in enumerate(songs, start=1):
+        mark = "*" if song[3] == UNLEARNED else " "
+        print(f"{i}. {mark} {song[0]:30} - {song[1]:20} ({song[2]})")
+
+    print(f"\n{len(songs) - unlearned_count} songs learned, {unlearned_count} songs still to learn.")
 
 def add_songs(songs):
     """Add a new song to the song list."""
